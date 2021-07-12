d21a7b650719be8afdeca591805d5618cf6a54da
Joekin Roche <joekinroches@gmail.com>
2021-07-12 13:50:58 +0530

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component{
+  constructor(props)
+  {
+super(props)
+this.state={
+  name:"Joekin",
+};
+  }
+  ChangeName = () => {
+    setInterval(()=>{
+      this.setState(
+        {
+          name: "John",
+        }
+      )
+    }, 10000);
+  
+}
+render()
+{this.ChangeName();
+  return(
+  <h1>Hello {this.state.name}</h1>)
+};
 }
 
-export default App;
+
+
